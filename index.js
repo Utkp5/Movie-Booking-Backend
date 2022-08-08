9d02c60344a4e9b3d33e1a857648e29d04d2f45a
PanditUtkarsh <panditutkarsh555@gmail.com>
2022-08-08 13:20:55 +0530

    backend

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 const logger = require("morgan");
 var cors = require('cors')
 
