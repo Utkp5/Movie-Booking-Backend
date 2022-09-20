2bed53ae4c5cc99b42236d8e9f78c5978da74814
Utkp5 <utkp200@gmail.com>
2022-09-21 01:55:28 +0530

    backend

--- routes/routes.js
+++ routes/routes.js
@@ -203,7 +203,26 @@ router.post("/Moviebook/:movieid", authFile.authenticationChecker, async(req,res
         }
     });
     
+    router.post("/verify", async (req, res) => {
+        try {
+            const { razorpay_order_id, razorpay_payment_id, razorpay_signature } =
+                req.body;
+            const sign = razorpay_order_id + "|" + razorpay_payment_id;
+            const expectedSign = crypto
+                .createHmac("sha256", process.env.KEY_SECRET)
+                .update(sign.toString())
+                .digest("hex");
     
+            if (razorpay_signature === expectedSign) {
+                return res.status(200).json({ message: "Payment verified successfully" });
+            } else {
+                return res.status(400).json({ message: "Invalid signature sent!" });
+            }
+        } catch (error) {
+            res.status(500).json({ message: "Internal Server Error!" });
+            console.log(error);
+        }
+    });
 
 
 
